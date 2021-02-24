eab350e0b379457c432a3462e54c3c05a47b17f1
PratyushSawan <mamtapriya22@gmail.com>
2021-02-24 17:34:30 +0530

    Working Version

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
-const affiliateLink = require('./views/index')
+const affiliateLink = require('./index')
 
 require('dotenv').config();
 
 const app = express()
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true})
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const connection = mongoose.connection;
-connection.once('open',() => {
-    console.log("MongoDB database connection established sucessfully");
+connection.once('open', () => {
+  console.log("MongoDB database connection established sucessfully");
 })
 
 // mongoose.connect("mongodb://localhost:/urls",{ useNewUrlParser:true, useUnifiedTopology: true},(error)=>{
@@ -27,44 +27,42 @@ connection.once('open',() => {
 // 	}
 // });
 
-// mongoose.connect('mongodb+srv://urlshort:sawan123@cluster0-t2gej.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-//     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true});
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 
 app.get('/', async (req, res) => {
 
- const shortUrls = await ShortUrl.find()
- res.render('index', {shortUrls: shortUrls})
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
 
 })
 
-app.post('/createAffiliate', async( req, res) => {
-	console.log(req.body.inputurl)
-	console.log(affiliateLink(req.body.inputurl));
+app.post('/createAffiliate', async (req, res) => {
+  console.log(req.body.inputurl)
+  console.log(affiliateLink(req.body.inputurl));
 
 })
 
-app.post('/shortUrls', async (req,res) => {
-	
-	console.log(req.body.inputurl)
-	console.log(affiliateLink(req.body.inputurl));
-  await ShortUrl.create({full: affiliateLink(req.body.inputurl) })
+app.post('/shortUrls', async (req, res) => {
+
+  console.log(req.body.inputurl)
+  console.log(affiliateLink(req.body.inputurl));
+  await ShortUrl.create({ full: affiliateLink(req.body.inputurl) })
 
   res.redirect('/')
 })
 
 app.get('/:shortUrl', async (req, res) => {
- const shortUrl= await ShortUrl.findOne({ short: req.params.shortUrl })
-if(shortUrl == null) return res.sendStatus(404)
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
 
-shortUrl.clicks++
-shortUrl.save()
-res.redirect(shortUrl.full);
+  shortUrl.clicks++
+  shortUrl.save()
+  res.redirect(shortUrl.full);
 })
 
-app.listen(port || 5000 , () => {
-    console.log(`Server is running on port: ${port}`)
+app.listen(port || 5000, () => {
+  console.log(`Server is running on port: ${port}`)
 });
